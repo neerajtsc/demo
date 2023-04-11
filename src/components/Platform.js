40fbc2fd1e4c2ed808f950daf8c411522214fe42
Ashutosh Mishra <ashutosh.mishra@techsuperiors.com>
2023-04-11 12:49:04 +0530

    img section

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import style from "../../styles/main.module.scss"
 
 function Platform() {
   return (
     <>
       <div className="container">
         <div className=""></div>
-        <div className="home row">
-          <div className="col-md-6">
-            <Image src="images/picture1.png" />
+        <div className="home row my-5">
+          <div className="col-md-6 p-1">
+            <Image
+              src="images/picture1.png"
+              alt="desktop"
+              className={`${style.desktopimg}` }
+            />
           </div>
-          <div className="col-md-6">
-            <h1></h1>
+          <div className="col-md-6 p-2">
+            <div className="p-3">
+              <h3 className="py-2 fs-2"><span className="fw-light">STEP</span> <span className="text-primary">01/06</span></h3>
+              <h1 className={`py-1`}>HOME Page</h1>
+              <p className={`${style.platformfont} `}>
+                Get a bird’s eye view of the whole range of models that are
+                prebuilt &  </p>
+                <p className={`${style.platformfont}`}> customizable based on top industry use
+                cases/problems.</p>
+            
+            </div>
           </div>
         </div>
       </div>
